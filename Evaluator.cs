d110c9e34441dc6fcc11a4691e74ffb90afb5717
Ernesto González <112357917+ernestgonzalezv@users.noreply.github.com>
2023-10-24 16:35:34 -0400

    added errors

--- Evaluator.cs
+++ Evaluator.cs
@@ -1,121 +1,169 @@
-public class Evaluator {
-    
-    private readonly ExpressionSyntax _root;
+public abstract class ExpressionSyntax
+{
+  
+  public class UnaryExpressionSyntax : ExpressionSyntax
+  {
+    public SyntaxToken OperatorToken;
+    public ExpressionSyntax Expression;
 
-    public Evaluator(ExpressionSyntax root ){
-        this._root = root;
-    }
-
-    public double Evaluate()
+    public UnaryExpressionSyntax(SyntaxToken operatorToken, ExpressionSyntax expression)
     {
-        return EvaluateExpression(_root);
+      OperatorToken = operatorToken;
+      Expression = expression;
     }
 
-    public double EvaluateExpression(ExpressionSyntax root)
+    public override R GetChildren<R>(Children<R> U)
     {
+      return U.GetUnaryExpressionSyntax(this);
+    }
+  }
+  public class CallingExpressionSyntax : ExpressionSyntax
+  {
+    public SyntaxToken FunctionName;
+    public List<ExpressionSyntax> FunctionParameters;
+    public int NumberOfParameters => FunctionParameters.Count;
 
-        
-        if(root is LiteralExpressionSyntax n){
-            return (double)((n.LiteralToken.Value));
-        }
 
-        if (root is UnaryExpressionSyntax u)
-        {
-            var operand = EvaluateExpression(u.Operand);
-            switch (u.Operator.Kind){
-                case SyntaxKind.PlusToken:
-                    return (double)operand;
-                case SyntaxKind.MinusToken:
-                    return -(double)operand;
-                throw new Exception("bro");
-            }
-
-        }
-        if(root is BinaryExpressionSyntax b)
-        {
-            
-            
-            var right = EvaluateExpression(b.Right);
-            var left = EvaluateExpression(b.Left);
-            switch(b.Operator.Kind)
-            {
-                case SyntaxKind.PlusToken:
-                    return  (double)( left) + (double)( right) ;
-                case SyntaxKind.MinusToken:
-                    return (double)( left) - (double)( right) ;
-                case SyntaxKind.StarToken:
-                    return (double)( left) * (double)( right) ;
-                case SyntaxKind.SlashToken:
-                    return (double)( left) / (double)( right) ;
-                case SyntaxKind.ExponentialToken:
-                    return  Math.Pow((double)( left) , (double)( right));
-                default:
-                    return -1;
-                throw new Exception("bro");
-            }
-        }
-        if(root is ParenthesizedExpression p)
-        {
-            return EvaluateExpression(p.Expression);
-        }
-        if(root is FunctionExpressionCalling f) {
-            var e = 1.2;
-            if(f.Parameters!="")
-                e = EvaluateExpression(f.Expression);
-            switch(f.Text){
-                case "print":
-                    //var e = EvaluateExpression(f.Expression); 
-                    Console.WriteLine((e));
-                    return 0;
-                case "sin":
-            
-                    return Math.Sin((e));
-                case "cos": 
-                    
-                    return Math.Cos((e));
-                case "sqrt":
-                    
-                    return Math.Sqrt((e));
-                case "exp":
-                    
-                    return Math.Exp((e));
-                default :
-                    //check name 
-                    //separate parameters  and replace em 
-                    //evaluate
-                    
-                    if(Program.functions.ContainsKey(f.Text)){
-                        string line1 =Program.functions[f.Text].Item2;
-                        if(f.Parameters!=""){ // if u actually get parameters
-                            var parameters = Program.functions[f.Text].Item1.Split(",");
-                            
-                            var parameters1 = f.Parameters.Split(",");
-                            //for(int i =0 ; i<parameters1.Length;i++)Console.WriteLine(parameters1[i]);
-                            for(int i=0;i<parameters1.Length;i++){
-                                line1 = line1.Replace(parameters[i], parameters1[i]);
-                            }
-                        }
-                        var parser= new Parser(line1);
-                        parser.Parse();
-                        var expression = parser.ans; 
-                        return EvaluateExpression(expression);
-                    }else{
-                        Console.WriteLine(f.Text);
-                        throw new Exception("NO key for this function, not defined");
-                    }
-                    return -1;
-
-            }
-        }
-        if(root is AssignmentExpressionSyntax a)
-        {   
-            Console.WriteLine(a.Left +" = " + EvaluateExpression(a.Right));
-
-            Program.variables.Add(a.Left, a.Right.ToString());
-            //Console.WriteLine(EvaluateExpression(a.Right));
-            return 0 ;
-        } 
-        throw new Exception("bro");
+    public CallingExpressionSyntax(SyntaxToken functionName, List<ExpressionSyntax> functionParameters)
+    {
+      FunctionName = functionName;
+      FunctionParameters = functionParameters;
+    }
+    public override R GetChildren<R>(Children<R> C)
+    {
+      return C.GetCallingExpressionSyntax(this);
+    }
+  }
+  public class AssignmentExpressionSyntax : ExpressionSyntax
+  {
+    public ExpressionSyntax VariableValue;
+    public SyntaxToken VariableName;
+    public AssignmentExpressionSyntax(SyntaxToken variableName, ExpressionSyntax variableValue)
+    {
+      VariableValue = variableValue;
+      VariableName = variableName;
     }
 
+    public override R GetChildren<R>(Children<R> A)
+    {
+      return A.GetAssignmentExpressionSyntax(this);
+    }
+  }
+  public class LiteralExpressionSyntax : ExpressionSyntax
+  {
+    public object LiteralToken;
+    public LiteralExpressionSyntax(object literalToken)
+    {
+      LiteralToken = literalToken;
+    }
+
+    public override R GetChildren<R>(Children<R> L)
+    {
+      return L.GetLiteralExpressionSyntax(this);
+    }
+  }
+
+  public class FunctionExpressionSyntax : ExpressionSyntax
+  {
+    public SyntaxToken FunctionName;
+    public List<SyntaxToken> FunctionParameters;
+    public ExpressionSyntax FunctionBody;
+    public int NumberOfParameters => FunctionParameters.Count;
+    public bool CallAgain;
+
+    public FunctionExpressionSyntax(SyntaxToken functionName, List<SyntaxToken> functionParameters, 
+    ExpressionSyntax functionBody, bool callAgain = false)
+    {
+      FunctionName = functionName;
+      FunctionParameters = functionParameters;
+      FunctionBody = functionBody;
+      CallAgain = callAgain;
+    }
+    public override R GetChildren<R>(Children<R> F)
+    {
+      return F.GetFunctionExpressionSyntax(this);
+    }
+  }
+  public class VariableExpressionSyntax : ExpressionSyntax
+  {
+    public SyntaxToken VariableName;
+
+    public VariableExpressionSyntax(SyntaxToken variableName)
+    {
+      VariableName = variableName;
+    }
+
+    public override R GetChildren<R>(Children<R> V)
+    {
+      return V.GetVariableExpressionSyntax(this);
+    }
+  }
+
+  public class BinaryExpressionSyntax : ExpressionSyntax
+  {
+    public ExpressionSyntax Left;
+    public SyntaxToken OperatorToken;
+    public ExpressionSyntax Right;
+
+    public BinaryExpressionSyntax(ExpressionSyntax left, SyntaxToken operatorToken, ExpressionSyntax right)
+    {
+      Left = left;
+      OperatorToken = operatorToken;
+      Right = right;
+    }
+
+    public override R GetChildren<R>(Children<R> B)
+    {
+      return B.GetBinaryExpressionSyntax(this);
+    }
+  }
+  public class LetInExpressionSyntax : ExpressionSyntax
+  {
+    public List<AssignmentExpressionSyntax> VariableAssignments;
+    public ExpressionSyntax AfterIn;
+
+    public LetInExpressionSyntax(List<AssignmentExpressionSyntax> variableAssignments, ExpressionSyntax afterIn)
+    {
+      VariableAssignments = variableAssignments;
+      AfterIn = afterIn;
+    }
+
+    public override R GetChildren<R>(Children<R> L)
+    {
+      return L.GetLetInExpressionSyntax(this);
+    }
+  }
+  public class ConditionalExpressionSyntax : ExpressionSyntax
+  {
+    public ExpressionSyntax Condition;
+    public ExpressionSyntax Response;
+    public ExpressionSyntax ElseExpression;
+
+    public ConditionalExpressionSyntax(ExpressionSyntax condition, ExpressionSyntax response, ExpressionSyntax elseExpression)
+    {
+      Condition = condition;
+      Response = response;
+      ElseExpression = elseExpression;
+    }
+
+    public override R GetChildren<R>(Children<R> C)
+    {
+      return C.GetConditionalExpressionSyntax(this);
+    }
+  }
+  public abstract R GetChildren<R>(Children<R> visitor);
+  public interface Children<R>
+  {
+    R GetUnaryExpressionSyntax(UnaryExpressionSyntax UnaryExpression);
+    R GetFunctionExpressionSyntax(FunctionExpressionSyntax FunctionExpression);
+    R GetLiteralExpressionSyntax(LiteralExpressionSyntax LiteralExpression);
+    R GetCallingExpressionSyntax(CallingExpressionSyntax CallingExpression);
+    R GetAssignmentExpressionSyntax(AssignmentExpressionSyntax AssignmentExpression);
+    R GetVariableExpressionSyntax(VariableExpressionSyntax VariableExpression);
+    R GetLetInExpressionSyntax(LetInExpressionSyntax LetInExpression);
+    R GetBinaryExpressionSyntax(BinaryExpressionSyntax BinaryExpression);
+    R GetConditionalExpressionSyntax(ConditionalExpressionSyntax ConditionalExpression);
+  }
+
+  
 }
